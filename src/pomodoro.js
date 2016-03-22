a122a668bb9269914a50c2ae1f3b74af5b46ef65
Bradley Stachurski <bradley.stachurski@gmail.com>
2016-03-22 00:28:30 -0500

    Gotta get some sleep. Setup some foundations but need to clean up some messes.

--- src/pomodoro.js
+++ src/pomodoro.js
@@ -1,3 +1,102 @@
 /**
  * Created by Bradley on 3/21/16.
  */
+$(document).ready(function() {
+
+    var $play  = $('.play'),
+        $pause = $('.pause'),
+        $stop  = $('.stop'),
+        $time  = $('.time-input'),
+        $timerSeconds = $('.timerSeconds');
+    $timerMinutes = $('.timerMinutes');
+
+    var initialTime,
+        clockPercent;
+
+    var timer = new Timer({
+        onstart : function(milliseconds) {
+            var seconds = Math.round(milliseconds / 1000) % 60;
+            var minutes = Math.floor(Math.round(milliseconds / 1000) / 60);
+            $timerSeconds.text(seconds);
+            $timerMinutes.text(minutes);
+            clockPercent = (Math.round(milliseconds / 1000)) / initialTime;
+            console.log(clockPercent);
+            playSoundBegin()
+        },
+        ontick  : function(milliseconds) {
+            var seconds = Math.round(milliseconds / 1000) % 60;
+            var minutes = Math.floor(Math.round(milliseconds / 1000) / 60);
+            $timerSeconds.text(seconds);
+            $timerMinutes.text(minutes);
+            clockPercent = (Math.round(milliseconds / 1000)) / initialTime;
+            console.log(clockPercent);
+            //updateRadialTimer(clockPercent);
+            drawProgress(clockPercent);
+        },
+        onpause : function() {
+            $timerMinutes.text('');
+            $timerSeconds.text('pause');
+        },
+        onstop  : function() {
+            $timerMinutes.text('&nbsp');
+            $timerSeconds.text('stop');
+        },
+        onend   : function() {
+            $timerSeconds.text('end');
+            $timerMinutes.text('');
+            playSoundEnd();
+        }
+    });
+
+    $play.on('click', function() {
+        var time = $time.val();
+        initialTime = $time.val();
+        if (!time) return;
+        if (isNaN(time)) {
+            alert('Please input valid number');
+            return;
+        }
+
+        timer.start(time);
+    });
+
+    $pause.on('click', function() {
+        if (timer.getStatus() === 'started') {
+            timer.pause();
+        }
+    });
+
+    $stop.on('click', function() {
+        if (/started|paused/.test(timer.getStatus())) {
+            timer.stop();
+        }
+    });
+
+    function playSoundEnd() {
+        //var mySound = 'http://www.presentationmagazine.com/sound/bell_ting_ting.mp3';
+        //http://onlineclock.net/audio/options/harp-strumming.mp3
+        //http://onlineclock.net/audio/options/foghorn.mp3
+        var mySound = 'http://www.presentationmagazine.com/sound/bell_ting_ting.mp3';
+        var audio = new Audio(mySound);
+        audio.play();
+    }
+
+    function playSoundBegin() {
+        var mySound = 'http://www.soundjay.com/clock/clock-winding-1.mp3';
+        var audio = new Audio(mySound);
+        audio.play();
+    }
+
+    function updateRadialTimer(percentageRemaing) {
+        var $ppc = $('.progress-pie-chart'),
+            percent = percentageRemaing,
+            deg = 360 * percent / 100;
+
+        if (percent > 50) {
+            $ppc.addClass('gt-50');
+        }
+
+        $('.ppc-progress-fill').css('transform', 'rotate(' + deg + 'deg)');
+        $('.ppc-percents span').html(percent + '%');
+    }
+});
---
+++ src/progress.js
@@ -0,0 +1,62 @@
+/**
+ * Created by Bradley on 3/21/16.
+ */
+
+var clamp = function (n, min, max) {
+    return Math.max(min, Math.min(max, n));
+};
+
+var drawProgress = function(percent){
+
+    if(isNaN(percent)) {
+        return;
+    }
+
+    var width = 360;
+    var height = 360;
+    var radius = Math.min(width, height) / 2;
+    var donutWidth = 90;
+
+    var color = d3.scale.category20b();
+
+    var svg = d3.select('#colorado-flag')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .append('g')
+        .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
+
+    var arc = d3.svg.arc()
+        .innerRadius(radius - donutWidth)
+        .outerRadius(radius)
+        .startAngle(.65 * Math.PI)
+        .endAngle(2.35 * Math.PI);
+
+    var innerArc = d3.svg.arc()
+        .innerRadius(0)
+        .outerRadius(donutWidth)
+        .startAngle(0 * Math.PI)
+        .endAngle(1.5 * Math.PI);
+
+    svg.append('path')
+        .attr('d', arc)
+        .attr('fill', 'rgb(194, 27, 43)');
+
+    /*svg.append('circle')
+     .attr('cx', 0)
+     .attr('cy', 0)
+     .attr('r', 90)
+     .attr('fill', 'rgb(255, 235, 102)');*/
+
+    var countdown = svg.append('path')
+        .attr('d', innerArc)
+        .attr('fill', 'rgb(255, 217, 0');
+
+    percent = clamp(parseFloat(percent), 0, 1);
+
+};
+
+var max = 1;
+var progress = 0.0;
+
+drawProgress(progress);
